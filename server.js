a4c4604ca81065d337c6511f88991bd63de27cb3
vkasraj <vikas@klickinvite.com>
2018-07-24 14:52:49 +0530

    server: connected mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const { MONGO_URI } = require("./server/config/keys/index");
 
+/**
+ * First create a folder name db in C:\mongodb\data\
+ *
+ * cmd: "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --port 27017 --dbpath C:\mongodb\data\db
+ */
+
+/* Connecting MonogoDB */
+mongoose
+	.connect(
+		MONGO_URI,
+		{
+			useNewUrlParser: true
+		}
+	)
+	.then(() => console.log("MongoDB successfully connected!"))
+	.catch(e => console.log(e));
+
+/* Firing up Server */
 app.listen(PORT, () => {
 	console.log(`Server is up on port: ${PORT}`);
 });
