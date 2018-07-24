1919bdbe30a45f3627576cf52301cffad5effda6
vkasraj <vikas@klickinvite.com>
2018-07-24 15:41:26 +0530

    todo: added todo model

---
+++ server/models/todo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ToDo = mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	reminder: {
+		type: Date,
+		default: new Date().toUTCString()
+	},
+	author: {
+		type: mongoose.SchemaTypes.ObjectId,
+		ref: "User"
+	}
+});
+
+module.exports = mongoose.model("ToDo", ToDo);
