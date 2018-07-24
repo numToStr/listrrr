201cb37723a71332ce5848901a56d65880a921e9
vkasraj <vikas@klickinvite.com>
2018-07-24 15:32:01 +0530

    user: saving user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-	const { name } = req.body;
+const User = require("../models/user");
 
-	res.send({
-		welcome: name
+router.post("/", (req, res) => {
+	const { email } = req.body;
+
+	const U = new User({
+		email
 	});
+
+	User.findOne({ email })
+		.select("_id email")
+		.then(user => {
+			if (!user) {
+				U.save().then(({ _id, email }) => {
+					res.status(201).send({
+						user: { _id, email }
+					});
+				});
+			} else {
+				res.status(200).send({
+					user
+				});
+			}
+		});
 });
 
 module.exports = router;
