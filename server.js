3df25e13ca4f14a277a8c2515b06195406861810
vkasraj <vikas@klickinvite.com>
2018-07-26 16:14:16 +0530

    server: fixed deprecated sendfile => sendFile

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require("./server/middlewares/express")(app);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.resolve(__dirname, "client/build")));
 	app.get("*", (req, res) =>
-		res.sendfile(path.resolve(__dirname, "client/build", "index.html"))
+		res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
 	);
 }
 
