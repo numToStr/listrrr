65c5ce3964c6ce3c9b9295e921c139ba76c16155
vkasraj <vikasraj1911@gmail.com>
2019-02-21 16:59:37 +0530

    client::app: removed new styles install

--- client/src/root/App.js
+++ client/src/root/App.js
@@ -3,7 +3,7 @@ import { hot } from "react-hot-loader";
 import BrowserRouter from "react-router-dom/BrowserRouter";
 import { Provider } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import muiInstall from "@material-ui/styles/install";
+// import muiInstall from "@material-ui/styles/install";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 import configureStore from "../store/index.store";
@@ -13,7 +13,7 @@ import Root from "./Root";
 
 // Installing new styles solution by @material-ui/styles
 // Remove when it becomes stable
-muiInstall();
+// muiInstall();
 
 const store = configureStore();
 
