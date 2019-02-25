512f5b21a01264987df0b730eceb2f91d197ff02
vkasraj <vikasraj1911@gmail.com>
2019-02-25 12:05:00 +0530

    client::store: moved combineReducer function to index.reducer

--- client/src/store/index.store.js
+++ client/src/store/index.store.js
@@ -1,15 +1,13 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import http from "./middlewares/http.middleware";
 import reducers from "./reducers/index.reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const REDUCERS = combineReducers(reducers);
-
 export default function configureStore(initialState = {}) {
     return createStore(
-        REDUCERS,
+        reducers,
         initialState,
         composeEnhancers(applyMiddleware(http))
     );
--- client/src/store/reducers/index.reducer.js
+++ client/src/store/reducers/index.reducer.js
@@ -1,13 +1,15 @@
+import { combineReducers } from "redux";
+
 import http from "./http.reducer";
 import auth from "./auth.reducer";
 import issue from "./issue.reducer";
 import project from "./project.reducer";
 import templates from "./template.reducer";
 
-export default {
+export default combineReducers({
     http,
     auth,
     issue,
     project,
     templates
-};
+});
