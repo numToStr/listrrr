0f38e18d61c5f2aa931b154a29060e2896b668ce
vkasraj <vikasraj1911@gmail.com>
2019-02-26 18:45:28 +0530

    client::form: added spacing to form layout

--- client/src/components/forms/form.wrapper.js
+++ client/src/components/forms/form.wrapper.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const FormWrapper = ({ children }) => {
+const styles = ({ spacing }) => ({
+    root: {
+        marginTop: spacing.unit * 10
+    }
+});
+
+const FormWrapper = ({ classes, children }) => {
     return (
-        <Grid container justify="center">
+        <Grid container justify="center" className={classes.root}>
             <Grid item xs={9} sm={5} md={3}>
                 {children}
             </Grid>
@@ -11,4 +18,4 @@ const FormWrapper = ({ children }) => {
     );
 };
 
-export default FormWrapper;
+export default withStyles(styles)(FormWrapper);
