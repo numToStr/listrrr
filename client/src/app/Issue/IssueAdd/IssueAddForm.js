c95ffa8e0033568f33afb4e91a4f9df77b241b2b
vkasraj <vikasraj1911@gmail.com>
2019-04-21 20:14:38 +0530

    client::issue: add form connected to store

--- client/src/app/Issue/IssueAdd/IssueAddForm.js
+++ client/src/app/Issue/IssueAdd/IssueAddForm.js
@@ -1,58 +1,90 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { Field, Form } from "formik";
+import { connect } from "react-redux";
 import Grid from "@material-ui/core/Grid";
 
 import InputField from "../../../components/Form/FormFields/FormTextField";
 import SelectField from "../../../components/Form/FormFields/FormSelect";
 import LoadingButton from "../../../components/Form/FormFields/FormButton";
 import FormLayout from "../../../components/Form/FormLayout";
+import Loader from "../../../components/Loader/Loader";
 
-const IssueAddForm = ({ onSubmit, initialValues, loading, options }) => (
-    <FormLayout
-        onSubmit={onSubmit}
-        initialValues={initialValues}
-        render={({ dirty }) => (
-            <Form>
-                <Grid container spacing={16}>
-                    <Grid item xs={9}>
-                        <Field
-                            name="title"
-                            label="Title"
-                            type="text"
-                            component={InputField}
-                        />
-                        <Field
-                            name="description"
-                            label="Description"
-                            type="text"
-                            multiline
-                            rows={6}
-                            rowsMax={10}
-                            component={InputField}
-                        />
-                        <Grid container justify="flex-end">
-                            <LoadingButton
-                                loading={loading}
-                                disabled={!dirty || loading}
-                                fullWidth={false}
-                            >
-                                Submit
-                            </LoadingButton>
+import { issueAdd, projectList } from "../../../store/actions/index.action";
+
+const initialValues = { title: "", description: "", project: "" };
+
+const IssueAddForm = ({ $issueAdd, $projectList, _projectList, _loading }) => {
+    const $$projectList = useCallback($projectList, []);
+
+    useEffect(() => {
+        $$projectList();
+    }, []);
+
+    if (!_projectList) {
+        return <Loader />;
+    }
+
+    return (
+        <FormLayout
+            key="issue-add-form"
+            onSubmit={$issueAdd}
+            initialValues={initialValues}
+            render={({ dirty }) => (
+                <Form>
+                    <Grid container spacing={16}>
+                        <Grid item xs={9}>
+                            <Field
+                                name="title"
+                                label="Title"
+                                type="text"
+                                component={InputField}
+                            />
+                            <Field
+                                name="description"
+                                label="Description"
+                                type="text"
+                                multiline
+                                rows={6}
+                                rowsMax={10}
+                                component={InputField}
+                            />
+                            <Grid container justify="flex-end">
+                                <LoadingButton
+                                    loading={_loading}
+                                    disabled={!dirty || _loading}
+                                    fullWidth={false}
+                                >
+                                    Submit
+                                </LoadingButton>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Field
+                                name="project"
+                                type="text"
+                                label="Add to project"
+                                component={SelectField}
+                                options={_projectList}
+                            />
                         </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Field
-                            name="project"
-                            type="text"
-                            label="Add to project"
-                            component={SelectField}
-                            options={options}
-                        />
-                    </Grid>
-                </Grid>
-            </Form>
-        )}
-    />
-);
+                </Form>
+            )}
+        />
+    );
+};
 
-export default IssueAddForm;
+const mapStateToProps = ({ http: { request }, project }) => ({
+    _loading: request.issueAdd,
+    _projectList: project.list
+});
+
+const mapDispatchToProps = {
+    $issueAdd: issueAdd,
+    $projectList: projectList
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(IssueAddForm);
--- client/src/app/Issue/IssueAdd/IssueAddIndex.js
+++ client/src/app/Issue/IssueAdd/IssueAddIndex.js
@@ -1,84 +1,24 @@
-import React, { Fragment, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import withStyles from "@material-ui/core/styles/withStyles";
+import React, { Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
-import BackIcon from "@material-ui/icons/ArrowBackTwoTone";
 
-import { issueAdd, projectList } from "../../../store/actions/index.action";
-
-import Loader from "../../../components/Loader/Loader";
 import IssueAddForm from "./IssueAddForm";
+import HeaderBackButton from "../../../components/Header/HeaderBackButton";
 
-const initialValues = { title: "", description: "", project: "" };
-
-const styles = ({ spacing }) => ({
-    headerMargin: {
-        marginBottom: spacing.unit * 2
-    }
-});
-
-const _Link = props => <Link to="/d/issues/list" {...props} />;
-
-const IssueAdd = ({
-    classes,
-    $issueAdd,
-    $projectList,
-    _loadingIssueAdd,
-    _projectList
-}) => {
-    useEffect(() => {
-        $projectList();
-    }, []);
-
-    const onSubmit = values => $issueAdd(values);
-
-    if (!_projectList) {
-        return <Loader />;
-    }
-
-    return (
-        <Fragment>
-            <Grid container justify="space-between" alignItems="center">
-                <Grid item xs={12}>
-                    <IconButton
-                        color="primary"
-                        component={_Link}
-                        className={classes.headerMargin}
-                    >
-                        <BackIcon fontSize="small" />
-                    </IconButton>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="h5" gutterBottom>
-                        New Issue
-                    </Typography>
-                </Grid>
+const IssueAdd = () => (
+    <Fragment>
+        <Grid container justify="space-between" alignItems="center">
+            <Grid item xs={12}>
+                <HeaderBackButton to="/d/issues/list" />
             </Grid>
-            <IssueAddForm
-                key="issue-add-form"
-                onSubmit={onSubmit}
-                initialValues={initialValues}
-                loading={_loadingIssueAdd}
-                options={_projectList}
-            />
-        </Fragment>
-    );
-};
+            <Grid item xs={12}>
+                <Typography variant="h5" gutterBottom>
+                    New Issue
+                </Typography>
+            </Grid>
+        </Grid>
+        <IssueAddForm />
+    </Fragment>
+);
 
-const mapStateToProps = ({ http: { request }, project }) => ({
-    _loadingIssueAdd: request.issueAdd,
-    _projectList: project.list
-});
-
-const mapDispatchToProps = {
-    $issueAdd: val => issueAdd(val),
-    $projectList: () => projectList()
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withStyles(styles)(IssueAdd));
+export default IssueAdd;
