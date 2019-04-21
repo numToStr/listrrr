6a4f7c517c34e3c8faa49f237cb072608d88dd6d
vkasraj <vikasraj1911@gmail.com>
2019-04-21 14:49:05 +0530

    client::project: using FormLayout in add project and add issue forms

--- client/src/app/Issue/IssueAdd/IssueAddForm.js
+++ client/src/app/Issue/IssueAdd/IssueAddForm.js
@@ -1,63 +1,58 @@
-// Note: I am not using FormLayout component because its layout is different
-
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Field, Form } from "formik";
 import Grid from "@material-ui/core/Grid";
 
 import InputField from "../../../components/Form/FormFields/FormTextField";
 import SelectField from "../../../components/Form/FormFields/FormSelect";
 import LoadingButton from "../../../components/Form/FormFields/FormButton";
+import FormLayout from "../../../components/Form/FormLayout";
 
-const IssueAddForm = ({ onSubmit, initialValues, loading, options }) => {
-    return (
-        <Formik
-            onSubmit={onSubmit}
-            validateOnBlur={false}
-            validateOnChange={false}
-            initialValues={initialValues}
-            render={({ dirty }) => (
-                <Form>
-                    <Grid container spacing={16}>
-                        <Grid item xs={9}>
-                            <Field
-                                name="title"
-                                label="Title"
-                                type="text"
-                                component={InputField}
-                            />
-                            <Field
-                                name="description"
-                                label="Description"
-                                type="text"
-                                multiline
-                                rows={6}
-                                rowsMax={10}
-                                component={InputField}
-                            />
-                            <Grid container justify="flex-end">
-                                <LoadingButton
-                                    loading={loading}
-                                    disabled={!dirty || loading}
-                                    fullWidth={false}
-                                >
-                                    Submit
-                                </LoadingButton>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Field
-                                name="project"
-                                type="text"
-                                label="Add to project"
-                                component={SelectField}
-                                options={options}
-                            />
+const IssueAddForm = ({ onSubmit, initialValues, loading, options }) => (
+    <FormLayout
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        render={({ dirty }) => (
+            <Form>
+                <Grid container spacing={16}>
+                    <Grid item xs={9}>
+                        <Field
+                            name="title"
+                            label="Title"
+                            type="text"
+                            component={InputField}
+                        />
+                        <Field
+                            name="description"
+                            label="Description"
+                            type="text"
+                            multiline
+                            rows={6}
+                            rowsMax={10}
+                            component={InputField}
+                        />
+                        <Grid container justify="flex-end">
+                            <LoadingButton
+                                loading={loading}
+                                disabled={!dirty || loading}
+                                fullWidth={false}
+                            >
+                                Submit
+                            </LoadingButton>
                         </Grid>
                     </Grid>
-                </Form>
-            )}
-        />
-    );
-};
+                    <Grid item xs={3}>
+                        <Field
+                            name="project"
+                            type="text"
+                            label="Add to project"
+                            component={SelectField}
+                            options={options}
+                        />
+                    </Grid>
+                </Grid>
+            </Form>
+        )}
+    />
+);
 
 export default IssueAddForm;
--- client/src/app/Project/ProjectAdd/ProjectAddForm.js
+++ client/src/app/Project/ProjectAdd/ProjectAddForm.js
@@ -1,48 +1,36 @@
-// Note: I am not using FormLayout component because its layout is different
-
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Form, Field } from "formik";
 import Grid from "@material-ui/core/Grid";
 
 import InputField from "../../../components/Form/FormFields/FormTextField";
 import SelectField from "../../../components/Form/FormFields/FormSelect";
 import LoadingButton from "../../../components/Form/FormFields/FormButton";
+import FormLayout from "../../../components/Form/FormLayout";
 
-const ProjectAddForm = ({ onSubmit, initialValues, loading, templates }) => {
-    return (
-        <Formik
-            onSubmit={onSubmit}
-            validateOnBlur={false}
-            validateOnChange={false}
-            initialValues={initialValues}
-            render={({ dirty }) => (
-                <Form>
-                    <Field
-                        name="title"
-                        label="Title"
-                        type="text"
-                        component={InputField}
-                    />
-                    <Field
-                        name="description"
-                        label="Description"
-                        type="text"
-                        multiline
-                        rows={6}
-                        rowsMax={10}
-                        component={InputField}
-                    />
-                    <Grid container justify="space-between">
-                        <Grid item xs={4}>
-                            <Field
-                                name="template"
-                                type="text"
-                                label="Project Template"
-                                component={SelectField}
-                                options={templates}
-                            />
-                        </Grid>
-                        <Grid item>
+const ProjectAddForm = ({ onSubmit, initialValues, loading, templates }) => (
+    <FormLayout
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        render={({ dirty }) => (
+            <Form>
+                <Grid container spacing={16}>
+                    <Grid item xs={9}>
+                        <Field
+                            name="title"
+                            label="Title"
+                            type="text"
+                            component={InputField}
+                        />
+                        <Field
+                            name="description"
+                            label="Description"
+                            type="text"
+                            multiline
+                            rows={6}
+                            rowsMax={10}
+                            component={InputField}
+                        />
+                        <Grid container justify="flex-end">
                             <LoadingButton
                                 loading={loading}
                                 disabled={!dirty || loading}
@@ -52,10 +40,19 @@ const ProjectAddForm = ({ onSubmit, initialValues, loading, templates }) => {
                             </LoadingButton>
                         </Grid>
                     </Grid>
-                </Form>
-            )}
-        />
-    );
-};
+                    <Grid item xs={3}>
+                        <Field
+                            name="template"
+                            type="text"
+                            label="Project Template"
+                            component={SelectField}
+                            options={templates}
+                        />
+                    </Grid>
+                </Grid>
+            </Form>
+        )}
+    />
+);
 
 export default ProjectAddForm;
