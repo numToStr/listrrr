494480247aa79f501c6a478cd43df8366d711b1e
vkasraj <vikasraj1911@gmail.com>
2019-05-03 16:31:21 +0530

    client: moved to cra v3

--- v3/src/components/DragAndDrop/DropContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-
-const DropContext = ({ onDragEnd, children }) => {
-    return <DragDropContext onDragEnd={onDragEnd}>{children}</DragDropContext>;
-};
-
-export default DropContext;
