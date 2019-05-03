bf637bcd21104bb0e2fa6a54babaa9a441d03a8d
vkasraj <vikasraj1911@gmail.com>
2019-05-03 12:17:11 +0530

    client:header: added search bar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,6 +6,10 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import Box from "@material-ui/core/Box";
+import SearchIcon from "@material-ui/icons/SearchTwoTone";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 const useStyles = makeStyles(({ spacing }) => ({
     addIcon: {
@@ -22,25 +26,43 @@ const Header = ({ addLink, title }) => {
 
     return (
         <Box mb={3}>
-            <Grid
-                container
-                justify="space-between"
-                alignItems="center"
-                className={classes.headerMargin}
-            >
+            <Grid container justify="space-between" alignItems="center">
                 <Grid item>
                     <Typography variant="h5">{title}</Typography>
                 </Grid>
-                <Grid item>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        component={_Link}
-                    >
-                        <AddIcon className={classes.addIcon} />
-                        Add
-                    </Button>
+                <Grid item xs={6}>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item xs>
+                            <TextField
+                                placeholder="Search"
+                                fullWidth
+                                InputProps={{
+                                    disableUnderline: true,
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <IconButton
+                                                color="primary"
+                                                type="submit"
+                                            >
+                                                <SearchIcon />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="small"
+                                component={_Link}
+                            >
+                                <AddIcon className={classes.addIcon} />
+                                Add
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Box>
