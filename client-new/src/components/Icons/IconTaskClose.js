b7f490e7bebec8ec5bc3f2fef0b8b7074069e77a
vkasraj <vikasraj1911@gmail.com>
2019-10-18 15:18:25 +0530

    migration complete

--- client-new/src/components/Icons/IconTaskClose.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import DoneIcon from "@material-ui/icons/DoneAllTwoTone";
-
-const IconTaskClose = props => <DoneIcon fontSize="small" {...props} />;
-
-export default IconTaskClose;
