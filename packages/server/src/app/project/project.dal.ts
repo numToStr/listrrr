2eefec0ebe9fdf133e45f9968e18277efe79b312
numToStr <vikasraj1911@gmail.com>
2020-02-29 23:33:26 +0530

    fix(issue): not removing issue from old columns

--- packages/server/src/app/project/project.dal.ts
+++ packages/server/src/app/project/project.dal.ts
@@ -2,17 +2,24 @@ import { Types } from "mongoose";
 import { Project, ProjectModel } from "./project.schema";
 import { RootDAL } from "../../utils/fns/root.dal";
 
+interface ProjectColumn {
+    columnID: Types.ObjectId;
+}
+
+type CTX = Partial<Project | Record<string, unknown>>;
+
 export class ProjectDAL extends RootDAL<Project> {
-    constructor(ctx: Partial<Project | Record<string, unknown>> = {}) {
+    constructor(ctx: CTX = {}) {
         super(ProjectModel, ctx);
     }
 
     static async columns(projectIDs: Array<Types.ObjectId>) {
-        const columnsArray = await ProjectModel.aggregate()
+        const columnsArray = await ProjectModel.aggregate<ProjectColumn>()
             .match({
                 _id: {
                     $in: projectIDs,
                 },
+                closed: false,
             })
             .project({
                 _id: 0,
